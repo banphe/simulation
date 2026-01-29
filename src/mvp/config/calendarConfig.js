69ea0ebf9eb0e90bc48a7fb51edfcd601632beeb
kubek <jd707@outlook.com>
2026-01-29 17:44:36 +0100

    Set FullCalendar content height

--- src/mvp/config/calendarConfig.js
+++ src/mvp/config/calendarConfig.js
@@ -1,5 +1,7 @@
 export const getCalendarOptions = () => ({
     initialView: 'dayGridMonth',
+    height: 'auto',
+    contentHeight: '80dvh',
     headerToolbar: {
         left: 'prev,next today',
         center: 'title',
