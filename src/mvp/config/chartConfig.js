1d541a735a046fec77a62b915dca970c4bc83615
kubek <jd707@outlook.com>
2026-01-29 18:36:46 +0100

    Set report chart height to 100%

--- src/mvp/config/chartConfig.js
+++ src/mvp/config/chartConfig.js
@@ -1,5 +1,5 @@
 export const getChartOptions = (data) => ({
-    chart: { type: 'bar', height: '90%', toolbar: { show: false } },
+    chart: { type: 'bar', height: '100%', toolbar: { show: false } },
     plotOptions: { bar: { borderRadius: 4, horizontal: true } },
     colors: ['#1e40af'],
     grid: { padding: { top: -20 } },
